8b5a1d654f4050cb6cec7b9f2236e833372db383
yoavsraya <118468638+yoavsraya@users.noreply.github.com>
2024-05-08 16:20:35 +0300

    xxx

--- NodeJs/Server.js
+++ NodeJs/Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const dotenv = require('dotenv').config({ path: './Code-Analyzer/NodeJs/.env' });
+const dotenv = require('dotenv').config({ path: './NodeJs/.env' });
 if (dotenv.error)
 {
   throw dotenv.error;
