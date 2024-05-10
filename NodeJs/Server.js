3373f72e25cabec876ea57110744eb60f8adeb27
yoavsraya <118468638+yoavsraya@users.noreply.github.com>
2024-05-10 18:33:11 +0300

    add gitHubApi

--- NodeJs/Server.js
+++ NodeJs/Server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const { Octokit } = require("@octokit/rest");
+
+let octokit;
+let user;
+class User {
+  constructor(accessToken, userName, repositories) {
+    this.accessToken = accessToken;
+    this.userName = userName;
+    this.repositories = repositories;
+    this.selectedRepo = null;
+  }
+}
 
 const dotenv = require('dotenv').config({ path: './NodeJs/.env' });
 if (dotenv.error)
 {
-  throw dotenv.error;
+  const dotenv = require('dotenv').config();
+  if (dotenv.error)
+    {
+      throw dotenv.error;
+    }
 }
 const port = process.env.PORT || 3000;
 
@@ -32,10 +48,53 @@ app.get(`/${process.env.CALLBACK_URL}`, async (req, res) => {
     const userData = await GitHubApi.exchangeCodeForToken(code);
     console.log(userData);
     res.send('Successfully authenticated!');
-  } catch (error) {
+    
+    octokit = new Octokit({
+      auth: `${userData.access_token}`,
+    });
+    
+    const { data } = await octokit.rest.users.getAuthenticated();
+    const username = data.login;
+
+    octokit.rest.repos.listForAuthenticatedUser()
+    .then(({ data }) => {
+      user = new User(userData.access_token,username,data);
+      const repoNames = data.map(repo => repo.name);
+      console.log(repoNames); // This will log an array of repository names
+      user.selectedRepo = data[0];
+    })
+    .catch(error => {
+      console.error('Error getting repositories:', error);
+    });
+  }
+  catch (error) {
     console.error('Error during authentication:', error);
     res.status(500).send('Authentication failed');
   }
 });
+
+app.post('/getRepoFiles', (req, res) => {
+  const repoName = req.body.repoName;
+
+  const octokit = new Octokit({
+    auth: user.accessToken, // Use the user's access token here
+  });
+
+  octokit.rest.repos.getContent({
+    owner: user.username, // Use the user's GitHub username here
+    repo: repoName,
+    path: '', // Use an empty string to get the root directory
+  })
+  .then(({ data }) => {
+    const fileNames = data.map(file => file.name);
+    console.log(fileNames); // This will log an array of file names
+    res.send(fileNames);
+  })
+  .catch(error => {
+    console.error('Error getting files:', error);
+    res.status(500).send('Error getting files');
+  });
+});
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
