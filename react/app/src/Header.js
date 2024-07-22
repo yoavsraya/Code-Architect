c5821a50e6127ddb2c797894dcc07c7bafaf913a
yoavsraya <sraya.yoav@gmail.com>
2024-07-22 08:36:35 +0000

    head header

--- react/app/src/Header.js
+++ react/app/src/Header.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './Header.css';
 import logo from './logo3.png';
 import logo2 from './logo4.png';
+import githubLogo from './github-logo.png'; // Add your GitHub logo image
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-container-left">
-        <img src={logo} alt="Logo" className="logo" />
+      <div className="left-section">
+        <img src={logo} alt="Logo" className="TextLogo" />
+        <img src={logo2} alt="Logo" className="Logo" />
       </div>
-      <div className="logo-container-right">
-        <img src={logo2} alt="Logo 2" className="logo" />
+      <div className="right-section">
+        <a href="https://github.com/yoavsraya/Code-Analyzer" target="_blank" rel="noopener noreferrer">
+          <img src={githubLogo} alt="GitHub" className="github-logo" />
+        </a>
       </div>
     </header>
   );
